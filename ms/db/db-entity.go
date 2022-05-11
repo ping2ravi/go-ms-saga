0dca8deb3f7e258359bfa9d4729c7aec5cdc6b9e
Ravi Sharma <ping2ravi@gmail.com>
2022-05-11 08:32:00 +0100

    First

--- ms/db/db-entity.go
+++ ms/db/db-entity.go
@@ -6,8 +6,8 @@ type ApiRequest struct {
 	ID            int64     `gorm:"column:id"`
 	BusinessTxnId string    `gorm:"column:business_txn_id"`
 	ApiRequestKey string    `gorm:"column:api_request_key"`
-	StartTime     time.Time `gorm:"column:date_created"`
-	EndTime       time.Time `gorm:"column:date_modified"`
+	StartTime     time.Time `gorm:"column:start_time"`
+	EndTime       time.Time `gorm:"column:end_time"`
 	Ver           int32     `gorm:"column:ver"`
 	ApiUrl        string    `gorm:"column:api_url"`
 	Status        string    `gorm:"column:status"`
